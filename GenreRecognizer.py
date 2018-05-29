d8e8a62eda6c4f3c55ba832d67c7856291a00759
Jakub Kucharz <ruseknobusek@wp.pl>
2018-05-30 01:06:58 +0200

    20 songs
    20  30s songs in ech genre
    fixed test/train split

--- GenreRecognizer.py
+++ GenreRecognizer.py
@@ -27,7 +27,7 @@ def extract_features_song(f): #normalizacja danych (-1,1) dla sieci neuronowych
 def generate_features_and_labels():
     all_features = []
     all_labels = []
-    genres =['blues','classical', 'country', 'disco', 'dubstep', 'jazz', 'metal', 'pop', 'reagge', 'rock']
+    genres =['classical', 'country', 'disco', 'dubstep', 'jazz', 'metal', 'pop','rap', 'reagge', 'rock']
     for genre in genres:
         sound_files = glob.glob('genres/'+genre+'/*.wav')
         print('Processing %d songs in %s genre...' %(len(sound_files), genre))
@@ -35,14 +35,17 @@ def generate_features_and_labels():
             features = extract_features_song(f)
             all_features.append(features)
             all_labels.append(genre)
+    #onehot encoding
     label_uniq_ids, label_row_ids = np.unique(all_labels, return_inverse=True)
     label_row_ids = label_row_ids.astype(np.int32, copy=False)
     onehot_labels = to_categorical(label_row_ids,len(label_uniq_ids))
     return np.stack(all_features), onehot_labels
+
+
 features, labels =generate_features_and_labels()
 
-print(np.shape(features))
-print(np.shape(labels))
+print('Features:', np.shape(features)) 
+print('Labels: ', np.shape(labels))
 training_split = 0.8
 alldata = np.column_stack((features, labels))
 np.random.shuffle(alldata)
@@ -50,24 +53,27 @@ splitidx = int(len(alldata) * training_split)
 train, test = alldata[:splitidx,:], alldata[splitidx:,:]
 
 #print rozmiarow tablic, zmiania rozmiaru o 10 (ze stackowania)
-print(np.shape(train))
-print(np.shape(test))
-train_input = test[:,:-10]
+print('Train shape: ',np.shape(train))
+print('Test shape: ',np.shape(test))
+train_input = train[:,:-10]
 train_labels = train[:,-10:]
+
 test_input = test[:,:-10]
 test_labels = test[:,-10:]
 print(np.shape(train_input))
 print(np.shape(train_labels))
     
-
+#siec neuronowa
 model = Sequential([
-        Dense(100, input_dim=np.shape(train_input)[1]),
+        Dense(20, input_dim=np.shape(train_input)[1]),
         Activation('relu'),
         Dense(10),
         Activation('softmax')
         ])
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 print(model.summary())
+print('dupa')
+
 model.fit(train_input, train_labels, epochs=10, batch_size=32, validation_split=0.2)
 loss, accuracy = model.evaluate(test_input, test_labels, batch_size=32)
 print("Loss: %.4f, accuracy: %.4F" %(loss, accuracy))
